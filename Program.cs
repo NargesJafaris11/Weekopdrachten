d857aaabe1d543d425b01237528e1e28f587a417
nargesjafari <nargesjafarie@gmail.com>
2026-06-24 10:06:52 +0200

    Added week assignment 2 if else

--- Program.cs
+++ Program.cs
@@ -92,7 +92,39 @@ static void Weekopdracht1()
 static void Weekopdracht2()
 {
     Console.Clear();
-    Console.WriteLine("Weekopdracht 2");
+    Console.WriteLine("Weekopdracht 2: If-Else");
+    Console.Write("Voer een cijfer in van 1,0 t/m 10,0: ");
+
+    bool isValidNumber = double.TryParse(Console.ReadLine(), out double grade);
+
+    if (!isValidNumber || grade < 1.0 || grade > 10.0)
+    {
+        Console.WriteLine("Ongeldige invoer. Voer een getal in van 1,0 t/m 10,0.");
+        Pause();
+        return;
+    }
+
+    if (grade < 4.0)
+    {
+        Console.WriteLine("Slecht");
+    }
+    else if (grade < 5.5)
+    {
+        Console.WriteLine("Matig");
+    }
+    else if (grade < 7.0)
+    {
+        Console.WriteLine("Voldoende");
+    }
+    else if (grade < 8.5)
+    {
+        Console.WriteLine("Goed");
+    }
+    else
+    {
+        Console.WriteLine("Uitstekend");
+    }
+
     Pause();
 }
 
